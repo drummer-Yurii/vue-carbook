40e7d2cc2913bf412543a7aa07c0419161c786d1
Yurii Rybachok <drummer2009@ukr.net>
2025-03-12 18:39:02 +0200

    carcards component data with cars for futured section

--- src/components/homePage/FeaturedSection.vue
+++ src/components/homePage/FeaturedSection.vue
@@ -1,7 +1,12 @@
 <script setup>
 import SectionHeading from '@/components/composables/SectionHeading.vue';
+import CarCards from '@/components/cards/CarCards.vue';
+import { cars } from '@/data/carsData';
 </script>
 
 <template>
     <SectionHeading description="What we offer" title="Featured Vechicles" />
+    <div v-for="(car, index) in cars" :key="index">
+        <CarCards :id="car.id" :name="car.name" :brand="car.brand" :price="car.price" :image="car.image" />
+    </div>
 </template>

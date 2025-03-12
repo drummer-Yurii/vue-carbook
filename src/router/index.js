be90b14d9b4f94a5c1d3ca7c817c1ee8b881e422
Yurii Rybachok <drummer2009@ukr.net>
2025-03-12 15:47:48 +0200

    create other pages & add them to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import DefaultLayout from '@/components/layout/DefaultLayout.vue';
 import HomePage from '@/page/HomePage.vue';
+import AboutPage from '@/page/AboutPage.vue';
+import ServicesPage from '@/page/ServicesPage.vue';
+import CarsPage from '@/page/CarsPage.vue';
+import ContactPage from '@/page/ContactPage.vue';
+import BlogPage from '@/page/BlogPage.vue';
+import PricingPage from '@/page/PricingPage.vue';
 
 const routes = [
   {
@@ -11,6 +17,36 @@ const routes = [
         path: '',
         name: 'home',
         component: HomePage,
+      },
+      {
+        path: '/about',
+        name: 'about',
+        component: AboutPage,
+      },
+      {
+        path: '/services',
+        name: 'services',
+        component: ServicesPage,
+      },
+      {
+        path: '/cars',
+        name: 'cars',
+        component: CarsPage,
+      },
+      {
+        path: '/contact',
+        name: 'contact',
+        component: ContactPage,
+      },
+      {
+        path: '/blog',
+        name: 'blog',
+        component: BlogPage,
+      },
+      {
+        path: '/pricing',
+        name: 'pricing',
+        component: PricingPage,
       },
     ],
   },

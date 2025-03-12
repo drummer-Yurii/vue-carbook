9dd56ad17cc6fdd14abba9bb3bb2fdd223e53a42
Yurii Rybachok <drummer2009@ukr.net>
2025-03-12 09:51:47 +0200

    crate main layout pages for project

---
+++ src/components/layout/DefaultLayout.vue
@@ -0,0 +1,9 @@
+<script setup>
+import PageHeader from '@/components/layout/PageHeader.vue';
+import PageFooter from '@/components/layout/PageFooter.vue';
+</script>
+
+<template>
+    <PageHeader />
+    <PageFooter />
+</template>

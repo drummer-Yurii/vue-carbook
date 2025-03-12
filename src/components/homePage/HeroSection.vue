3a393b8998b2890337e80d8a1f3cbcb1ba3baa35
Yurii Rybachok <drummer2009@ukr.net>
2025-03-12 16:13:47 +0200

    content in hero section

--- src/components/homePage/HeroSection.vue
+++ src/components/homePage/HeroSection.vue
@@ -4,9 +4,16 @@ import bgImage from '../../assets/images/bg_1.jpg';
 
 <template>
   <section class="bg-cover relative h-screen" :style="{ backgroundImage: `url(${bgImage})` }">
-    <!-- <div class="container">
-      <h1 class="text-center">Welcome to My Website</h1>
-      <p class="text-center">This is a template for a simple website.</p>
-    </div> -->
+    <div class="absolute inset-0 bg-black opacity-25"></div>
+    <div class="relative flex items-center justify-center h-full text-center text-white px-4">
+      <div class="w-full md:w-3/5">
+        <h1 class="text-4xl md:text-5xl font-bold mb-4">Fast & Ease Way To Rent A Car</h1>
+        <p class="text-md md:text-xl mb-6">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut quisquam doloremque sequi
+          dolorum voluptatum saepe.
+        </p>
+        <h2 class="text-2xl font-semibold">Ease steps for renting a car</h2>
+      </div>
+    </div>
   </section>
 </template>

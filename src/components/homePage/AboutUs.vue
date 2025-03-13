eab82540f241e7dd3a412f4d5a1166919d5ceaca
Yurii Rybachok <drummer2009@ukr.net>
2025-03-13 10:11:22 +0200

    ServiceList section for home page

--- src/components/homePage/AboutUs.vue
+++ src/components/homePage/AboutUs.vue
@@ -3,7 +3,7 @@ import ButtonComponent from '@/components/composables/ButtonComponent.vue';
 </script>
 
 <template>
-  <div class="relative w-full h-screen">
+  <div class="relative w-full md:h-screen">
     <div class="sm:absolute bg-[#10b981] w-full sm:w-2/3 md:h-[92%] top-0 right-0 pb-10">
       <div class="w-full sm:absolute -inset-x-1/3 inset-y-20 mb-4 md:mb-0">
         <img
--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -2,10 +2,12 @@
 import HeroSection from '@/components/homePage/HeroSection.vue';
 import FeaturedSection from '@/components/homePage/FeaturedSection.vue';
 import AboutUs from '@/components/homePage/AboutUs.vue';
+import ServiceList from '@/components/homePage/ServiceList.vue';
 </script>
 
 <template>
   <HeroSection />
   <FeaturedSection />
   <AboutUs />
+  <ServiceList />
 </template>

c1703f54d681e457b12443fe0bda97421d674daf
Yurii Rybachok <drummer2009@ukr.net>
2025-03-13 12:30:34 +0200

    Blog section with cards for home page

---
+++ src/components/cards/BlogCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+import ButtonComponent from '@/components/composables/ButtonComponent.vue';
+
+defineProps({
+  id: Number,
+  title: String,
+  author: String,
+  comments: Number,
+  date: String,
+  image: String,
+  description: String,
+});
+
+const getImage = (imageName) => {
+  return new URL(`/src/assets/images/${imageName}`, import.meta.url).href;
+};
+</script>
+
+<template>
+  <div>
+    <img :src="getImage(image)" alt="blog image" class="object-cover h-56 w-full" />
+    <div class="space-y-2 pt-5">
+      <p class="text-sm text-blue-500">{{ date }} {{ author }} {{ comments?.length }} comments</p>
+      <h2 class="text-xl font-medium text-gray-800 mt-2">{{ title }}</h2>
+      <ButtonComponent text="Read More" />
+    </div>
+  </div>
+</template>
--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -5,6 +5,7 @@ import AboutUs from '@/components/homePage/AboutUs.vue';
 import ServiceList from '@/components/homePage/ServiceList.vue';
 import CallToAction from '@/components/homePage/CallToAction.vue';
 import TestimonySection from '@/components/homePage/TestimonySection.vue';
+import BlogSection from '@/components/homePage/BlogSection.vue';
 </script>
 
 <template>
@@ -14,4 +15,5 @@ import TestimonySection from '@/components/homePage/TestimonySection.vue';
   <ServiceList />
   <CallToAction />
   <TestimonySection />
+  <BlogSection />
 </template>

b8a7a63dd875a4f48e08e1480ec8b9eb51ac37fb
Yurii Rybachok <drummer2009@ukr.net>
2025-03-14 14:14:29 +0200

    create About Page & Bg Container

--- src/page/AboutPage.vue
+++ src/page/AboutPage.vue
@@ -1,9 +1,22 @@
 <script setup>
-
+import BgContainer from '@/components/composables/BgContainer.vue';
+import AboutUs from '@/components/homePage/AboutUs.vue';
+import CallToAction from '@/components/homePage/CallToAction.vue';
+import TestimonySection from '@/components/homePage/TestimonySection.vue';
+import StatisticsSection from '@/components/homePage/StatisticsSection.vue';
 </script>
 
 <template>
-    <div>
-
-    </div>
+  <BgContainer
+    title="About"
+    bgImage="bg_3.jpg"
+    :links="[
+      { text: 'Home', to: '/' },
+      { text: 'About Us', to: '/about' },
+    ]"
+  />
+  <AboutUs />
+  <CallToAction />
+  <TestimonySection />
+  <StatisticsSection />
 </template>

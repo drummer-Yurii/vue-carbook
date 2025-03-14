1d5cbc55880eab7743ad5f59f19f3511414bf107
Yurii Rybachok <drummer2009@ukr.net>
2025-03-14 10:34:23 +0200

    footer page & footer links components

---
+++ src/components/composables/FooterLinks.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  title: String,
+  links: Array,
+});
+</script>
+
+<template>
+  <div>
+    <h2 class="text-white text-xl font-semibold mb-2">{{ title }}</h2>
+    <ul class="text-gray-400 space-y-3 mt-6">
+      <li v-for="(link, index) in links" :key="index" class="cursor-pointer text-md">{{ link }}</li>
+    </ul>
+  </div>
+</template>
